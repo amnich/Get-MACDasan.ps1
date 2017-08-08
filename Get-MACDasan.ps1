55ad27bc16f9b9cdecec409ef6fac341a9d8bcd1
Adam Mnich <adammnich@gmail.com>
2017-08-08 08:44:27 +0200

    Create Get-MACDasan.ps1

---
+++ Get-MACDasan.ps1
@@ -0,0 +1,26 @@
+function Get-MACDasan {
+    [CmdletBinding()]
+    param(	
+        [Parameter(Mandatory = $True)] 
+        [ValidateScript({$_ -match [IPAddress]$_ })] 
+        [string]$Ip,
+        [ValidateNotNull()]
+        [string]$Community = "public",
+        [string]$Snmpwalk = "C:\usr\bin\snmpwalk.exe" #path to snmpwalk.exe
+	
+    )	
+    $cmd = "$snmpwalk -v1 -c$community -Ox $Ip 1.3.6.1.4.1.6296.101.3.13.1.1.3"
+    Write-Verbose $cmd
+    $snmpResults = Invoke-Expression $cmd  #execute snmpwalk command
+    Write-Debug "$($snmpResults | Out-String)"
+    foreach ($single in $snmpResults) {
+        if ($single -match ('3\.13\.1\.1\.3\.(?<VLAN>\d{1,3})\.(?<Port>\d{1,3}).*= Hex-STRING: (?<MAC>.*)')) {
+            New-Object PSCustomObject -Property @{
+                Source = $ip
+                VLAN   = $Matches.VLAN
+                MAC    = $Matches.MAC.replace(' ', '')
+                Port   = $Matches.Port				
+            }			
+        }
+    }
+}
